0a702f386533e4a9ce4fd40c462a167d519f64e0
Jotason <90227459+Jotason@users.noreply.github.com>
2024-12-06 21:45:14 -0500

    061224Fin

---
+++ Assets/Scrips/ControladorPuntoDeGuardado.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ControladorPuntoDeGuardado : MonoBehaviour
+{
+    public static ControladorPuntoDeGuardado instancia;
+
+    [SerializeField] PuntoDeControl puntoActual;
+
+    private void Start()
+    {
+        if (instancia == null)
+        {
+            instancia = this;
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AgregarNuevoPunto(PuntoDeControl nuevoPunto) { 
+    
+        puntoActual = nuevoPunto;
+    
+    }
+}
