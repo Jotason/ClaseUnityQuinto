71cde6e19b18899b133fe973140e88aca06a3de3
Jotason <90227459+Jotason@users.noreply.github.com>
2025-02-25 21:24:47 -0500

    02252503

--- Assets/Scrips/Enemy/EstadoPatrullarEnemigo.cs
+++ Assets/Scrips/Enemy/EstadoPatrullarEnemigo.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EstadoPatrullarEnemigo : StateMachineBehaviour
 {
     ControladorEnemigo controlador;
+    [SerializeField] int destino = 0;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -16,9 +17,23 @@ public class EstadoPatrullarEnemigo : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        //Debug.Log("Esta patrullar");
-        controlador.Agt.SetDestination(controlador.PosicionesPatrullaje[0].position);
+        //Debug.Log(controlador.Agt.remainingDistance);
+        ////Debug.Log("Esta patrullar");
+        //controlador.Agt.SetDestination(controlador.PosicionesPatrullaje[0].position);
+        if (controlador.Agt.remainingDistance < 1) {
+            destino++;
 
+            if (destino > controlador.PosicionesPatrullaje.Count - 1) {
+                destino = 0;
+
+            }
+
+
+           controlador.Agt.SetDestination(controlador.PosicionesPatrullaje[destino].position);
+
+
+        }
+        //controlador.Agt.remainingDistance();
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
