cd4d14c3ea5aca516efe1f0f8a4fd2d146ac2ecf
Jotason <90227459+Jotason@users.noreply.github.com>
2025-02-27 21:35:16 -0500

    02272501Tempo

--- Assets/Scrips/Enemy/ControladorEnemigo.cs
+++ Assets/Scrips/Enemy/ControladorEnemigo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,20 +10,35 @@ public class ControladorEnemigo : MonoBehaviour
 
     public NavMeshAgent Agt { get => agente; set => agente = value; }
     private NavMeshAgent agente;
-    
-    
+
+    public bool Objetivo { get => objetivo; set => objetivo = value; }
+    [SerializeField] bool objetivo;
 
     [SerializeField] Transform pivoteCapsula;
     [SerializeField] Transform pivoteCapsula2;
+    [SerializeField] Transform pivotePatada;
+
     [SerializeField] float radio;
+    [SerializeField] float radioPatada;
+
     [SerializeField] LayerMask capasDeteccion;
+
+
+    public Collider[] Colisiones { get => colisiones; set => colisiones = value; }
     Collider[] colisiones;
-    [SerializeField] bool objetivo;
+
+    
+    
     [SerializeField]  float distancia;
 
+    [SerializeField] Vector3 fuerzaPatada;
 
     public List<Transform> PosicionesPatrullaje { get => posicionesPatrullaje; set => posicionesPatrullaje = value; }
+    
+
     [SerializeField] List<Transform> posicionesPatrullaje = new List<Transform>();
+
+    
 
 
     // Start is called before the first frame update
@@ -46,19 +62,19 @@ public class ControladorEnemigo : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        colisiones = Physics.OverlapCapsule(pivoteCapsula.position, pivoteCapsula2.position, radio, capasDeteccion);
+        Colisiones = Physics.OverlapCapsule(pivoteCapsula.position, pivoteCapsula2.position, radio, capasDeteccion);
         //objetivo = colisiones.Length>0 ? true : false;
-        if (colisiones.Length > 0)
+        if (Colisiones.Length > 0)
         {
-            objetivo = true;
-            distancia = Vector3.Distance(transform.position, colisiones[0].transform.position);
+            Objetivo = true;
+            distancia = Vector3.Distance(transform.position, Colisiones[0].transform.position);
         }
         else { 
         
-            objetivo= false;
+            Objetivo= false;
         }
 
-        anim.SetBool("Objetivo", objetivo);
+        anim.SetBool("Objetivo", Objetivo);
         anim.SetFloat("Distancia", distancia);
     }
 
@@ -68,7 +84,23 @@ public class ControladorEnemigo : MonoBehaviour
         Gizmos.DrawWireSphere(pivoteCapsula.position,radio);
         Gizmos.DrawWireSphere(pivoteCapsula2.position ,radio);
 
+        Gizmos.color= Color.green;
+        Gizmos.DrawWireSphere(pivotePatada.position, radioPatada);
+
+
     }
+
+    public void Empujar() {
+
+        Collider[] colisionesPatada = Physics.OverlapSphere(pivotePatada.position, radioPatada, capasDeteccion);
+        if (colisionesPatada.Length > 0) {
+            Vector3 direccion = colisionesPatada[0].transform.position - transform.position;
+            Vector3 fuerza = new Vector3(direccion.x * fuerzaPatada.x,fuerzaPatada.y,0);
+            colisionesPatada[0].GetComponent<Rigidbody>().AddForce(fuerza, ForceMode.Impulse);
+        
+        }
+    }
+
 
 
 }
